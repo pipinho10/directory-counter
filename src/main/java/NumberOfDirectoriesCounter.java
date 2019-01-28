866ceb6a4ae45f3ebbf5306f85e90c97c4b54ac6
pipinho <eltjon.sulaj@gmail.com>
2019-01-28 14:34:10 +0100

    Remove unused methods

--- src/main/java/NumberOfDirectoriesCounter.java
+++ src/main/java/NumberOfDirectoriesCounter.java
@@ -1,24 +1,23 @@
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveTask;
 
+import static java.util.Arrays.*;
+
 public class NumberOfDirectoriesCounter extends RecursiveTask<Integer> {
-    private static final int THRESHOLD = 2;
+    private static final int THRESHOLD = 10;
 
     private final File[] subDirectories;
 
-    public NumberOfDirectoriesCounter(File... subDirectories) {
+    private NumberOfDirectoriesCounter(File... subDirectories) {
         this.subDirectories = subDirectories;
     }
 
     public static void main(String[] args) {
-        String path = "C:\\Users\\esula";
-
-        NumberOfDirectoriesCounter numberOfDirectoriesCounter = new NumberOfDirectoriesCounter(getChildNodes(new File(path)));
+        NumberOfDirectoriesCounter numberOfDirectoriesCounter = new NumberOfDirectoriesCounter(getChildNodes(new File(args[0])));
 
         System.out.println(numberOfDirectoriesCounter.compute());
     }
@@ -31,7 +30,7 @@ public class NumberOfDirectoriesCounter extends RecursiveTask<Integer> {
                     .mapToInt(ForkJoinTask::join)
                     .sum();
         } else {
-            return getNumberOfFiles(subDirectories);
+            return calculateNumberOfFiles(subDirectories);
         }
     }
 
@@ -39,41 +38,25 @@ public class NumberOfDirectoriesCounter extends RecursiveTask<Integer> {
         List<NumberOfDirectoriesCounter> dividedTasks = new ArrayList<>();
 
         dividedTasks.add(new NumberOfDirectoriesCounter(
-                Arrays.copyOfRange(subDirectories, 0, subDirectories.length / 2)
+                copyOfRange(subDirectories, 0, subDirectories.length / 2)
         ));
 
         dividedTasks.add(new NumberOfDirectoriesCounter(
-                Arrays.copyOfRange(subDirectories, subDirectories.length / 2, subDirectories.length)
+                copyOfRange(subDirectories, subDirectories.length / 2, subDirectories.length)
         ));
 
         return dividedTasks;
     }
 
-    private static int getNumberOfFiles(File[] directories) {
+    private int calculateNumberOfFiles(File[] nodes) {
         int numberOfFiles = 0;
 
-        for (File node : directories) {
+        for (File node : nodes) {
             if (node.isDirectory()) {
-                for (File childNode : getChildNodes(node)) {
-                    numberOfFiles += getNumberOfFiles(childNode);
-                }
+                numberOfFiles += calculateNumberOfFiles(getChildNodes(node));
             } else {
                 numberOfFiles++;
             }
-        }
-
-        return numberOfFiles;
-    }
-
-    private static int getNumberOfFiles(File node) {
-        int numberOfFiles = 0;
-
-        if (node.isDirectory()) {
-            for (File childNode : getChildNodes(node)) {
-                numberOfFiles += getNumberOfFiles(childNode);
-            }
-        } else {
-            numberOfFiles++;
         }
 
         return numberOfFiles;
