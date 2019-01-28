4f9c97ce3009503f4269e7d2680dddf38a03cd50
pipinho <eltjon.sulaj@gmail.com>
2019-01-28 13:36:46 +0100

    Use RecursiveTask for concurrency

--- src/main/java/NumberOfDirectoriesCounter.java
+++ src/main/java/NumberOfDirectoriesCounter.java
@@ -1,18 +1,76 @@
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
 
-public class NumberOfDirectoriesCounter {
-    private static final String PATH = "C:\\Users\\esula\\Desktop\\New folder";
+public class NumberOfDirectoriesCounter extends RecursiveTask<Integer> {
+    private static final int THRESHOLD = 2;
 
-    public static void main(String[] args) {
-        System.out.println(getNumberOfFiles(new File(PATH)));
+    private final File[] subDirectories;
+
+    public NumberOfDirectoriesCounter(File... subDirectories) {
+        this.subDirectories = subDirectories;
     }
 
-    private static int getNumberOfFiles(File node){
+    public static void main(String[] args) {
+        String path = "C:\\Users\\esula";
+
+        NumberOfDirectoriesCounter numberOfDirectoriesCounter = new NumberOfDirectoriesCounter(getChildNodes(new File(path)));
+
+        System.out.println(numberOfDirectoriesCounter.compute());
+    }
+
+    @Override
+    public Integer compute() {
+        if (subDirectories.length > THRESHOLD) {
+            return ForkJoinTask.invokeAll(createSubtasks())
+                    .stream()
+                    .mapToInt(ForkJoinTask::join)
+                    .sum();
+        } else {
+            return getNumberOfFiles(subDirectories);
+        }
+    }
+
+    private Collection<NumberOfDirectoriesCounter> createSubtasks() {
+        List<NumberOfDirectoriesCounter> dividedTasks = new ArrayList<>();
+
+        dividedTasks.add(new NumberOfDirectoriesCounter(
+                Arrays.copyOfRange(subDirectories, 0, subDirectories.length / 2)
+        ));
+
+        dividedTasks.add(new NumberOfDirectoriesCounter(
+                Arrays.copyOfRange(subDirectories, subDirectories.length / 2, subDirectories.length)
+        ));
+
+        return dividedTasks;
+    }
+
+    private static int getNumberOfFiles(File[] directories) {
+        int numberOfFiles = 0;
+
+        for (File node : directories) {
+            if (node.isDirectory()) {
+                for (File childNode : getChildNodes(node)) {
+                    numberOfFiles += getNumberOfFiles(childNode);
+                }
+            } else {
+                numberOfFiles++;
+            }
+        }
+
+        return numberOfFiles;
+    }
+
+    private static int getNumberOfFiles(File node) {
         int numberOfFiles = 0;
 
         if (node.isDirectory()) {
-            for (String childNode : getChildNodes(node)) {
-                numberOfFiles += getNumberOfFiles(new File(node, childNode));
+            for (File childNode : getChildNodes(node)) {
+                numberOfFiles += getNumberOfFiles(childNode);
             }
         } else {
             numberOfFiles++;
@@ -21,10 +79,10 @@ public class NumberOfDirectoriesCounter {
         return numberOfFiles;
     }
 
-    private static String[] getChildNodes(File node) {
-        String[] childNodes = node.list();
+    private static File[] getChildNodes(File node) {
+        File[] childNodes = node.listFiles();
         if (childNodes == null) {
-            return new String[0];
+            return new File[0];
         }
         return childNodes;
     }
